903b762a938d35d76ac60f1927ee6b49aaa0fb89
vhieu-nx <nguyenvhieu1208@gmail.com>
2021-07-09 22:52:08 +0700

    login page done and doing front end sign up,in

---
+++ src/Components/SignUp/SignUp.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './SignUp.css';
+class SignUp extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return (  );
+    }
+}
+ 
+export default SignUp;
