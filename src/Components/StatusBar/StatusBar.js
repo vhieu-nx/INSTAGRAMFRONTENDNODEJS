8e14bb981417ce2f3096ea58dac901331019aba6
vhieu-nx <nguyenvhieu1208@gmail.com>
2021-07-10 13:02:49 +0700

    done design the status bar

--- src/Components/StatusBar/StatusBar.js
+++ src/Components/StatusBar/StatusBar.js
@@ -6,16 +6,46 @@ import statusimg from "../../images/pp1.png";
 class StatusBar extends Component {
     constructor(props) {
         super(props);
-        this.state = {}
+        this.state = {
+            statusList: []
+        }
+    }
+    componentDidMount(){
+        this.getData();
+    }
+    getData = () => {
+        let data = [
+            {
+                "username": "anindya_bunny",
+                "imageURL": "../../images/pp1.png"
+            },
+            {
+                "username": "abcs",
+                "imageURL": "../../images/pp1.png"
+            },
+            {
+                "username": "qwe",
+                "imageURL": "../../images/pp1.png"
+            },
+            {
+                "username": "jyjj",
+                "imageURL": "../../images/pp1.png"
+            }
+        ]
+        this.setState({ statusList: data });
     }
     render() {
         return (
             <div >
                 <div className="statusbar__container">
-                    <div className="status">
-                        <Avatar className="statusbar__status" src={statusimg} />
-                        <div className="statusbar__text">Hieu</div>
-                    </div>
+                    {
+                        this.state.statusList.map((item, index) => (
+                            <div className="status">
+                                <Avatar className="statusbar__status" src={statusimg} />
+                                <div className="statusbar__text">{item.username}</div>
+                            </div>
+                        ))
+                    }
                 </div>
             </div>
         );
