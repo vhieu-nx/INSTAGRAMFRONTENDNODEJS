cccdd21d1c33fa8147d47b23f38e54d29a212114
vhieu-nx <nguyenvhieu1208@gmail.com>
2021-07-10 14:02:21 +0700

    done front end using nodejs

--- src/Components/MainContent/MainContent.js
+++ src/Components/MainContent/MainContent.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import Grid from '@material-ui/core/Grid';
 import StatusBar from '../StatusBar/StatusBar';
 import MainPage from '../MainPage/MainPage';
+import InforSection from '../InforSection/InforSection';
+import Suggestions from '../Suggestions/Suggestions';
 class MainContent extends Component {
     constructor(props) {
         super(props);
@@ -18,7 +20,10 @@ class MainContent extends Component {
                             <MainPage />
                         </div>
                     </Grid>
-                    <Grid item xs={2}>dasasd</Grid>
+                    <Grid item xs={2}>
+                        <InforSection />
+                        <Suggestions />
+                    </Grid>
                     <Grid item xs={2}>dsada</Grid>
                 </Grid>
             </div>
--- src/Components/MainPage/MainPage.js
+++ src/Components/MainPage/MainPage.js
@@ -1,15 +1,68 @@
 import React, { Component } from 'react';
+import Post from '../Post/Post';
 import "./MainPage.css";
+import uploadImage from "../../images/upload.png";
 class MainPage extends Component {
     constructor(props) {
         super(props);
-        this.state = {  }
+        this.state = {
+            postArray: []
+        }
     }
-    render() { 
-        return ( 
-            <div>Post</div>
-         );
+    componentDidMount(){
+        this.getPost();
+    }
+    getPost = () => { //API
+        const thisContext = this;
+        let data = [
+            {
+                "postId": "123456",
+                "userName": "anindya",
+                "postImageURL": "https://irixlens.com/new/wp-content/uploads/2018/11/IRX_5473.jpg",
+                "timeStamp": "12345",
+                "likes": "1234"
+            },
+            {
+                "postId": "123456",
+                "userName": "anindya",
+                "postImageURL": "https://irixlens.com/new/wp-content/uploads/2018/11/IRX_5473.jpg",
+                "timeStamp": "12345",
+                "likes": "1234"
+            },
+            {
+                "postId": "123456",
+                "userName": "anindya",
+                "postImageURL": "https://irixlens.com/new/wp-content/uploads/2018/11/IRX_5473.jpg",
+                "timeStamp": "12345",
+                "likes": "1234"
+            }
+        ];
+        thisContext.setState({postArray: data});
+    }
+    render() {
+        return (
+            <div>
+                <div className="mainpage__container">  
+                    <div className="mainpage__divider"></div>
+                    <div className="fileupload">
+                        <label for="file-upload" >
+                            <img className="mainpage__uploadicon" src={uploadImage} />
+                        </label>
+                         <input onChange={this.upload} id="file-upload" type="file"/>
+                     </div>
+                    <div className="mainpage__divider"></div>   
+                </div>
+                <div className="upload_text">{this.state.progressBar}</div>
+               {
+                    this.state.postArray.map((item,index)=>(
+                        <Post id={item.postId} userName={item.userName} postImage={item.postImageURL} likes={item.likes} />
+                    ))
+                }
+            </div>
+
+
+        );
     }
 }
- 
+
 export default MainPage;
